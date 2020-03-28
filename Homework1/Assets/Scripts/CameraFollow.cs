da651adf7494006e628a33998e9b75f48cd748ee
Fatmagyul <fatmagyul.chitakova@gmail.com>
2020-03-28 09:54:05 +0300

    first homework - in progress

---
+++ Homework1/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public const string PLAYER_OBJECT_NAME = "PlayerBody";
+
+    [SerializeField]
+    Transform followedObjectTransform;
+    float speed = 0.01f;
+    Vector3 offsetFromPlayer;
+    
+    void Start()
+    {
+        followedObjectTransform = GameObject.Find(PLAYER_OBJECT_NAME).transform;
+        offsetFromPlayer = transform.position - followedObjectTransform.transform.position;
+    }
+
+    void LateUpdate()
+    { 
+        Vector3 desiredPosition = followedObjectTransform.position + offsetFromPlayer;
+        transform.position = desiredPosition;
+    }
+}
---
+++ Homework1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	[SerializeField]
+	private float speed = 3;
+
+	void FixedUpdate() {
+		Vector3 moveDirection =
+				new Vector3(Input.GetAxis("Horizontal"),
+							0,
+							Input.GetAxis("Vertical"))
+							.normalized
+				* Time.deltaTime
+				* speed;
+		Vector3 pointToLookAt = transform.position
+						  + moveDirection * 100;
+
+		transform.position += moveDirection;
+		transform.LookAt(pointToLookAt);
+	}
+}

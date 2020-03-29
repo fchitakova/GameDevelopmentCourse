705a2fb6479b49a4fb32c35832763fb436cf4620
Fatmagyul <fatmagyul.chitakova@gmail.com>
2020-03-29 11:34:02 +0300

    add simultaneusly jump and move to cube object

--- Homework2/Assets/Script/JumpingCube.cs
+++ Homework2/Assets/Script/JumpingCube.cs
@@ -2,29 +2,37 @@
 
 public class JumpingCube : MonoBehaviour
 {
+    const string PLANE_OBJECT_NAME = "Plane";
+
     bool isGrounded;
-    Rigidbody rigidbodyBody;
+    Rigidbody rigidBody;
 
     void Start()
     {
         isGrounded = true;
-        rigidbodyBody = GetComponent<Rigidbody>();
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         if (isGrounded)
         {
-            if (Input.GetKey(KeyCode.Space)){
-                rigidbodyBody.velocity = new Vector3(0f, 5f, 0f);
+            Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+            if (Input.GetKey(KeyCode.Space))
+            {
+                rigidBody.velocity = new Vector3(0f, 5f, 0f) + moveDirection;
                 isGrounded = false;
+            }
+            else
+            {
+                transform.position += moveDirection;
             }
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name.Equals("Plane"))
+        if (collision.gameObject.name.Equals(PLANE_OBJECT_NAME))
         {
             isGrounded = true;
         }

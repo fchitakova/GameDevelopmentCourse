b499ae3bc3dad27c2962b3495b6caeb4a041e3e2
Fatmagyul <fatmagyul.chitakova@gmail.com>
2020-03-29 10:57:27 +0300

    Homework 2

---
+++ Homework2/Assets/Script/JumpingCube.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class JumpingCube : MonoBehaviour
+{
+    bool isGrounded;
+    Rigidbody rigidbodyBody;
+
+    void Start()
+    {
+        isGrounded = true;
+        rigidbodyBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (isGrounded)
+        {
+            if (Input.GetKey(KeyCode.Space)){
+                rigidbodyBody.velocity = new Vector3(0f, 5f, 0f);
+                isGrounded = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name.Equals("Plane"))
+        {
+            isGrounded = true;
+        }
+    }
+
+}
